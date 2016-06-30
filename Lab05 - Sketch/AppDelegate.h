3df9a229b9de20fd6da8c3e15ffaba0c190fe1df
Haydee <haydee@haydees-imac.local>
2016-06-30 12:11:48 -0500

    Initial Commit

---
+++ Lab05 - Sketch/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Lab05 - Sketch
+//
+//  Created by Haydee on 6/30/16.
+//  Copyright © 2016 Haydee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Lab05 - Sketch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab05 - Sketch
+//
+//  Created by Haydee on 6/30/16.
+//  Copyright © 2016 Haydee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
